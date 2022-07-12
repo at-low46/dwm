68aea19332ebaf42d12b37f3d43657da87fd0b55
at-low46 <105537348+at-low46@users.noreply.github.com>
2022-07-11 23:47:21 -0300

    Update rules

--- config.h
+++ config.h
@@ -34,8 +34,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/*{ "Gimp",     NULL,       NULL,       0,            1,           -1 },*/
+	/*{ "Firefox",      NULL,    NULL,       1 << 8,       0,           -1 },*/
+	{ NULL, "Godot_Engine",    NULL,            0,       1,           -1 },
+	{ NULL, "mojosetup",    NULL,            0,       1,           -1 },
 };
 
 /* layout(s) */
